b048ea63a560301443b2ffd19237857962e1f5a8
Tiago Santos <tiagossilva_95@hotmail.com>
2018-01-10 23:54:28 +0000

    Ramp Picker View Controller creation and popover.

--- RampUp/Controllers/RampPlacerViewController.swift
+++ RampUp/Controllers/RampPlacerViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class RampPlacerViewController: UIViewController, ARSCNViewDelegate {
+class RampPlacerViewController: UIViewController, ARSCNViewDelegate, UIPopoverPresentationControllerDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -75,6 +75,20 @@ class RampPlacerViewController: UIViewController, ARSCNViewDelegate {
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+    
+    @IBAction func onRampButtonPressed(_ sender: UIButton) {
+        let rampPickerViewController = RampPickerViewController(size: CGSize(width: 250, height: 250))
+        rampPickerViewController.modalPresentationStyle = .popover
+        rampPickerViewController.popoverPresentationController?.delegate = self
+        present(rampPickerViewController, animated: true, completion: nil)
+        rampPickerViewController.popoverPresentationController?.sourceView = sender
+        rampPickerViewController.popoverPresentationController?.sourceRect = sender.bounds
         
     }
 }
