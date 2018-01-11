b5b66def17b8aee7e8d5efed658b4acdacf67609
Tiago Santos <tiagossilva_95@hotmail.com>
2018-01-11 00:40:16 +0000

    Added the skate ramp to the pop over of Ramp Picker View Controller.

--- RampUp/Controllers/RampPickerViewController.swift
+++ RampUp/Controllers/RampPickerViewController.swift
@@ -29,6 +29,18 @@ class RampPickerViewController: UIViewController {
         sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         view.insertSubview(sceneView, at: 0)
         
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        sceneView.scene = scene
+        
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
+        let object = SCNScene(named: "art.scnassets/pipe.dae")
+        let node = object?.rootNode.childNode(withName: "pipe", recursively: true)!
+        node?.scale = SCNVector3Make(0.0022, 0.0022, 0.0022)
+        node?.position = SCNVector3Make(-1, 0.7, -1)
+        scene.rootNode.addChildNode(node!)
         preferredContentSize = size
     }
 
